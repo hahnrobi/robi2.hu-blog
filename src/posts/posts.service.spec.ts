3ffdaafacef2ccafdecaa23ceb0f11fd0bb10593
Hahn Robi <robitwo.r2@gmail.com>
2022-02-08 18:08:15 +0100

    wanted to make a test

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import { Post } from './post.model';
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
