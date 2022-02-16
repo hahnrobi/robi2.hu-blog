faaec350ad1d5e33dbbaa0c23b596f7cbf6877b1
Hahn Robi <robitwo.r2@gmail.com>
2022-02-16 12:51:47 +0100

    Docker working

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,24 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './posts/posts.module';
 
+export const configuration = () => ({
+  NODE_ENV: process.env.NODE_ENV,
+  port: parseInt(process.env.PORT, 10) || 3000,
+  DATABASE_CONN: process.env.DATABASE_CONN
+});
+
 @Module({
   imports: [
-    ConfigModule.forRoot({ envFilePath: `.env` }),
+    ConfigModule.forRoot({ envFilePath: `.env` , load: [configuration]}),
     MongooseModule.forRoot(process.env.DATABASE_CONN),
     PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log("Running path: ", __dirname);
+    console.log("Database Connection string: " + process.env.DATABASE_CONN);
+  }
+}
