45dd01cb6b6e670290ac53a74b41aae648edfafd
Hahn Robi <robitwo.r2@gmail.com>
2022-04-05 20:08:32 +0200

    init

--- src/spotify/spotify.service.ts
+++ src/spotify/spotify.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 export class SpotifyService {
     // TODO: These will be moved to env variables.
     private readonly apiUrl = "https://api.spotify.com/v1";
-    private readonly token = "AQCR4iBfpzN0LmfxOkGvcmha84WVLGzmGM2QWL7ClfOn7WE8jL_sCVWXxH2ENAK9ApzW8a6XgiqQ5L1YY9tztwjJaL5NGCO36aur_aENiNei1Qf-IgkAWLeR8UjsVTAG72eEt1E2YT0xit_wTp5tjJZJQmqBZwFTtS0JaTQq_5ShZnECQVtQFfXNTA8TfQZWOw";
+    private readonly token = "";
     private readonly userId = "11125460307"
 
     constructor(private httpService: HttpService) {
