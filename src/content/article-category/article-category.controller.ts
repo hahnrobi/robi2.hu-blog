a348f703df2da3d083d5a04c1b8f7bb13ab71c9d
Hahn Robi <robitwo.r2@gmail.com>
2022-12-27 10:53:10 +0100

    article category controller url fixed

--- src/content/article-category/article-category.controller.ts
+++ src/content/article-category/article-category.controller.ts
@@ -5,7 +5,7 @@ import { CreateArticleCategoryDto } from '../models/articleCategory/dto/create-a
 import { UpdateArticleCategoryDto } from '../models/articleCategory/dto/update-articleCategory.dto';
 import { ArticleCategory } from '../models/articleCategory/entities/articleCategory.entity';
 
-@Controller('article-category')
+@Controller('content/article-category')
 export class ArticleCategoryController
     extends ContentController<
     CreateArticleCategoryDto,
