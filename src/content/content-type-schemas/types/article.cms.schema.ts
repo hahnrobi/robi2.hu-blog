8559299967daff1c07b72be73f5a846619c21f1d
Hahn Robi <robitwo.r2@gmail.com>
2022-12-27 16:22:04 +0100

    Draft layout works

--- src/content/content-type-schemas/types/article.cms.schema.ts
+++ src/content/content-type-schemas/types/article.cms.schema.ts
@@ -1,64 +1,83 @@
 import { SelectField } from './../fields/select';
 import { SlugField } from './../fields/slug';
 import { TitleField } from './../fields/title';
-import { ContentEditorLayoutColumn, ContentEditorLayoutContent, ContentTypeCmsSchema } from '../content-type.schema';
+import {
+  ContentEditorLayoutColumn,
+  ContentEditorLayoutContent,
+  ContentTypeCmsSchema,
+} from '../content-type.schema';
 import { TextField } from '../fields/text';
 
-
 export const articleContentCmsSchema: ContentTypeCmsSchema = {
-    listColumns: ['title', 'slug'],
-    searchable: false,
-    searchableFields: [],
-    draftable: false,
-    versioning: false,
-    fields: [
-        new TitleField(
-            'title',
-            'TITLE', 
-            [{name: 'required'}, {name: 'minLength', params: 2}]
-        ),
-        new SlugField(
-            'slug',
-            'SLUG', 
-            [{name: 'required'}, {name: 'minLength', params: 4}]
-        ),
-        new SelectField(
-            'categoryId',
-            'CATEGORY', 
-            [{name: 'required'}],
-            'article-category'
-        )
-    ],
-    editorLayout: {
-        type: 'row',
-        children: [
+  listColumns: ['title', 'slug'],
+  searchable: false,
+  searchableFields: [],
+  draftable: false,
+  versioning: false,
+  fields: [
+    new TitleField('title', 'TITLE', [
+      { name: 'required' },
+      { name: 'minLength', params: 2 },
+    ]),
+    new SlugField('slug', 'SLUG', [
+      { name: 'required' },
+      { name: 'minLength', params: 4 },
+    ]),
+    new SelectField(
+      'categoryId',
+      'CATEGORY',
+      [{ name: 'required' }],
+      'article-category',
+    ),
+  ],
+  editorLayout: [
+    {
+      type: 'row',
+      children: [
+        <ContentEditorLayoutColumn>{
+          type: 'column',
+          params: { width: '12' },
+          children: [
+            <ContentEditorLayoutContent>{
+              type: 'content',
+              itemType: 'field',
+              fieldName: 'title',
+            },
+          ],
+        },
+      ],
+    },
+    {
+          type: 'row',
+          children: [
             <ContentEditorLayoutColumn>{
-                type: 'column',
-                params: {width: "8"},
-                children: [
-                    <ContentEditorLayoutContent>{
-                        type: 'content',
-                        itemType: 'wysiwyg-1',
-                        fieldName: 'body'
-                    }
-                ]
+              type: 'column',
+              params: { width: '8' },
+              children: [
+                <ContentEditorLayoutContent>{
+                  type: 'content',
+                  itemType: 'wysiwyg-1',
+                  fieldName: 'body',
+                },
+              ],
             },
             <ContentEditorLayoutColumn>{
-                type: 'column',
-                params: {width: "4"},
-                children: [
-                    <ContentEditorLayoutContent>{
-                        type: 'content',
-                        itemType: 'field',
-                        fieldName: 'title'
-                    },
-                    <ContentEditorLayoutContent>{
-                        type: 'content',
-                        itemType: 'field',
-                        fieldName: 'slug'
-                    }
-                ]
-            }
-        ]
-    }
-}
+              type: 'column',
+              params: { width: '4' },
+              children: [
+                <ContentEditorLayoutContent>{
+                  type: 'content',
+                  itemType: 'field',
+                  fieldName: 'slug',
+                },
+                <ContentEditorLayoutContent>{
+                    type: 'content',
+                    itemType: 'field',
+                    fieldName: 'categoryId',
+                  },
+              ],
+            },
+          ],
+    },
+  ],
+};
