41ecc8c3791b70c112cdf040ead3344e56a8e40f
Hahn Robi <robitwo.r2@gmail.com>
2023-01-05 17:12:53 +0100

    article category fields to edit

--- src/content/content-type-schemas/types/article-category.cms.schema.ts
+++ src/content/content-type-schemas/types/article-category.cms.schema.ts
@@ -1,4 +1,6 @@
 import { ContentTypeCmsSchema } from '../content-type.schema';
+import { SlugField } from '../fields/slug';
+import { TitleField } from '../fields/title';
 
 
 export const articleCategoryCmsSchema: ContentTypeCmsSchema = {
@@ -8,13 +10,32 @@ export const articleCategoryCmsSchema: ContentTypeCmsSchema = {
           {
             fieldName: 'title',
             title: 'TITLE'
+          },
+          {
+            fieldName: '_count.articles',
+            title: 'ARTICLE_COUNT'
           }
         ]
       },
+
     searchable: false,
+
     searchableFields: [],
+
     draftable: false,
+
     versioning: false,
-    fields: [],
+
+    fields: [
+      new TitleField('title', 'TITLE', [
+        { name: 'required' },
+        { name: 'minLength', params: 2 },
+      ]),
+      new SlugField('slug', 'SLUG', [
+        { name: 'required' },
+        { name: 'minLength', params: 4 },
+      ]),
+    ],
+
     relationCounts: ['articles']
 }
